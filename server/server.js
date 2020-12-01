d237d554c1b27c8ee94216199e030e4e1c5a6724
Ryan Weaver <ryanweaver@Ryans-MacBook-Pro.local>
2020-12-01 14:33:28 -0700

    added cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 const cors = require('cors');
 
-app.use(express.static('../client/dist')); // this is the same is pulling up live server, actually IDK because live server is after it is bundled.
+app.use(express.static('../client/dist')); // this is the same is pulling up live server, they both should pull up index.html. and index.html has the bundle as a script tag. the bundle has all of the javascript tags inside of it, so it just packages it nicely for the index.html.
 app.use(express.json());
+app.use(cors());
 
 
 // Listening for requests on the PORT
