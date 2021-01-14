d42fcc437eb4006219bd08f9c93a8253a66bd97c
Ryan Weaver <rweaverra@gmail.com>
2021-01-14 13:32:45 -0700

    how it works section

--- client/components/homePageSections/HowitWorks.jsx
+++ client/components/homePageSections/HowitWorks.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 function HowitWorks () {
 
   return (
-    <div>2: How it Works</div>
+    <div>
+      <h1>How it Works</h1>
+      <div className="howItWorks">
+        <div className="howItWorksImage"></div>
+        <div className="howItWorksSteps">
+       <text> <strong>Step 1) Book Appointment \n</strong>
+                   you choose the time and location \n
+                   <strong>Step 2) Tech Arrives \n</strong>
+                    performs phone repair(typically 35  minutes)
+                    <strong>Step 3) You Pay</strong>
+                    after your device is fixed (cash, Venmo, or credit card)
+​                   <strong>Step 4) Bada boom bada bing!</strong>
+                   live your life because your phone is fixed
+                   </text>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/components/homePageSections/Intro.jsx
+++ client/components/homePageSections/Intro.jsx
@@ -7,7 +7,7 @@ function IntroSection () {
       <h1 className="header">Phone Repair: Where you want, When you want
 </h1>
 <h2 className="subHeader">Dont Stress, We will come to you!</h2>
-<button className="introButton">Book Appointment</button>
+<button className="introButton">Repair my Device</button>
 
     </div>
 
