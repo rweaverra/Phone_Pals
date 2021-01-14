6aee9365c8f3c815817730bf429256354b8a5cd6
Ryan Weaver <rweaverra@gmail.com>
2021-01-14 11:20:57 -0700

    book appointment button

---
+++ client/components/homePageSections/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Button() {
+
+  return (
+    <button className="introButton">Book Appointment</button>
+  );
+}
+
+
+export default Button;
+
--- client/components/homePageSections/Intro.jsx
+++ client/components/homePageSections/Intro.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Container, Row, Column} from 'react-bootstrap';
 
 function IntroSection () {
 
@@ -8,6 +7,8 @@ function IntroSection () {
       <h1 className="header">Phone Repair: Where you want, When you want
 </h1>
 <h2 className="subHeader">Dont Stress, We will come to you!</h2>
+<button className="introButton">Book Appointment</button>
+
     </div>
 
   )
