725b21b440ebff95d242a1bbd3895b1e6264a568
Ryan Weaver <rweaverra@gmail.com>
2021-01-18 15:39:52 -0700

    why choose section icons

--- client/components/homePageSections/WhyPhonePals.jsx
+++ client/components/homePageSections/WhyPhonePals.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaRegCreditCard } from 'react-icons/fa';
 import { AiOutlineFieldTime } from 'react-icons/Ai';
-import { GiNailedHead } from 'react-icons/Gi';
+import { BiHappy } from 'react-icons/Bi';
 
 function WhyPhonePals () {
 
@@ -10,22 +10,22 @@ function WhyPhonePals () {
       <h1 className="whyTitle">Why choose phone pals?</h1>
       <div className="whyIcons">
         <div className="whyIcon">
-          <FaRegCreditCard />
+          <FaRegCreditCard size={100}/>
           <h1>Save $</h1>
           ​No storefront = low overhead <br/>
-          we pass the savings on to you!
+          We pass the savings on to you!
         </div>
         <div className="whyIcon">
-          <AiOutlineFieldTime />
+          <AiOutlineFieldTime size={100}/>
           <h1>Save Time</h1>
           ​Phone repair on your time at your chosen location.<br/>
 ​          Repairs typically take 35 minutes.
         </div>
         <div className="whyIcon">
-          <GiNailedHead />
-          <h1>Save Headaches</h1>
-          Limited Lifetime Warrant <br />
-          Safe and Secure repair in front of you
+          <BiHappy size={100}/>
+          <h1>No Headaches</h1>
+          Limited Lifetime Warranty <br />
+          Rated A+ with BBB
         </div>
       </div>
 
