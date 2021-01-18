5dd8f05b458088ed869965c3005bb3d43550f7e6
Ryan Weaver <rweaverra@gmail.com>
2021-01-18 11:28:54 -0700

    why choose section

--- client/components/homePageSections/WhyPhonePals.jsx
+++ client/components/homePageSections/WhyPhonePals.jsx
@@ -6,14 +6,20 @@ function WhyPhonePals () {
     <div className="whyContainer">
       <h1 className="whyTitle">Why choose phone pals?</h1>
       <div className="whyIcons">
-        <div>
-          Save $
+        <div className="whyIcon">
+          <h1>Save $</h1>
+          ​No storefront = low overhead <br/>
+          we pass the savings on to you!
         </div>
-        <div>
-          Save Time
+        <div className="whyIcon">
+          <h1>Save Time</h1>
+          ​Phone repair on your time at your chosen location.<br/>
+​          Repairs typically take 35 minutes.
         </div>
-        <div>
-          Save Headaches
+        <div className="whyIcon">
+          <h1>Save Headaches</h1>
+          Limited Lifetime Warrant <br />
+          Safe and Secure repair in front of you
         </div>
       </div>
 
