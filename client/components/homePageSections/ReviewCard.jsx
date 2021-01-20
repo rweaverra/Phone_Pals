1164ff2eb913d2bac0240e37a86b5fe86410ad8c
Ryan Weaver <rweaverra@gmail.com>
2021-01-20 15:38:20 -0700

    review card

---
+++ client/components/homePageSections/ReviewCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AiFillStar } from 'react-icons/Ai';
+
+function ReviewCard() {
+
+  return (
+    <div>
+    4:  ReviewCard
+    <AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar />
+    </div>
+  );
+}
+
+export default ReviewCard;
--- client/components/homePageSections/Reviews.jsx
+++ client/components/homePageSections/Reviews.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReviewCard from './ReviewCard.jsx'
 
 function Reviews() {
 
   return (
     <div>
     4:  Reviews Section
+    <ReviewCard />
     </div>
   );
 }
